2e77f193b53c09a2d7b46f4da564be70efc39d58
Mahta <mahta@sprintasia.co.id>
2023-07-25 09:32:41 +0700

    update fungsi sayhelli

--- sayhello.go
+++ sayhello.go
@@ -1,5 +1,5 @@
-package sayhello
+package sayhello // nama ini yang akan di panggil buat di jadiin nama yang di panggil buat module
 
 func SayHello() string {
-	return "hello0"
+	return "hello world"
 }
